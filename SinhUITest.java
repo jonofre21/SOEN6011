0726b64adfa5ca1f7969e0198de063b5e017d828
jesus <jonofre21@gmail.com>
2019-07-26 22:53:22 -0400

    Unit tests java files - sinh Problem 4
    These files aims to test all the functions used during the implementation phase to achieve a high quality product.

---
+++ SinhUITest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SinhUITest {
+
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {}
+
+  @BeforeEach
+  void setUp() throws Exception {}
+
+  @Test
+  void test_UI() {
+    fail("Not yet implemented");
+  }
+
+}
