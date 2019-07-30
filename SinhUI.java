20f7f87c3713d44e7c67309a77a61b9424cab278
jesus <jonofre21@gmail.com>
2019-07-29 23:28:21 -0400

    Final Java files Function 3 sinh
    These are the latest java files for the sinh calculator program.

--- SinhUI.java
+++ SinhUI.java
@@ -1,172 +1,205 @@
 package sinh;
-import javax.swing.*;
-import javax.swing.border.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.script.ScriptEngineManager;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
-/*****************************************************************
-User Interface Class that will provide the main window to the user.
-
-@author Jesus Onofre Diaz
-@version 1.0
-*****************************************************************/
+/**
+ * User Interface Class that will provide the main window to the user.
+ * @author Jesus Onofre Diaz
+ * @version 1.0
+ */
 
 public class SinhUI extends JFrame implements ActionListener {
-        private static final int FRAME_WIDTH = 400;
-        private static final int FRAME_HEIGHT = 400;
-        private static final int FRAME_X = 550;
-        private static final int FRAME_Y = 550;
-        private JPanel buttonPanel;
-        private JPanel inputOutputPanel;
-        private JTextField value;
-        private boolean editable = true;
-        double value_of_x;
-        double x;
-        String val_of_x;
-        //SinhLogic logic = new SinhLogic();
+  private static final int FRAME_WIDTH = 400;
+  private static final int FRAME_HEIGHT = 400;
+  private static final int FRAME_X = 550;
+  private static final int FRAME_Y = 550;
+  private JPanel buttonPanel;
+  private JPanel inputOutputPanel;
+  private JPanel inputOutputPanel2;
+  private JTextField value;
+  private boolean editable = true;
+  double valuex;
+  double valueofx;
+  String finalxvalue;
         
-        /*****************************************************************
-        Default Constructor constructs the frame of the program
-        *****************************************************************/
+  /**
+  * Class Default Constructor.
+  */
+  public SinhUI()
+  {
+    //Defining a Container object
+    Container contentPane = getContentPane();
+    contentPane.setLayout(new FlowLayout());
+    
+    //Defining a JPanel object for the button object
+    buttonPanel = new JPanel();
+    buttonPanel.setLayout(new GridLayout(7,7,9,9));
+    buttonPanel.setBorder(new EmptyBorder(12,12,12,12));
+    
+    //Defining a JPanel object for the TextField object
+    inputOutputPanel = new JPanel();
+    inputOutputPanel.setLayout(new FlowLayout());
+    inputOutputPanel.setBorder(new EmptyBorder(40,40,40,40));
+    
+    //Defining a JPanel object for the Instruction label object
+    inputOutputPanel2 = new JPanel();
+    inputOutputPanel2.setLayout(new FlowLayout());
+    inputOutputPanel2.setBorder(new EmptyBorder(10,10,10,10));
+            
+    //Defining dimensions for the main frame
+    setTitle("SINH Calculator");
+    setSize(FRAME_WIDTH,FRAME_HEIGHT);
+    setResizable(false);
+    setLocation(FRAME_X,FRAME_Y);
+    
+    //Difining a label linked to the TextField object
+    JLabel label = new JLabel();        
+    label.setText("SINH :");
+    label.setBounds(40, 40, 200, 200);
+    inputOutputPanel.add(label);
+    
+    //Defining a label object for showing the instructions.       
+    JLabel label2 = new JLabel();        
+    label2.setText("<html> INPUT INSTRUCTIONS FOR SINH FUNCTION : <br> Negative and Positive Real Numbers. <br> Use Backspace to clear and type another value.</html>");
+    label2.setBounds(10, 10, 10, 10);
+    inputOutputPanel2.add(label2);
+    contentPane.add(inputOutputPanel2);
+    label2.setFont(new Font("Arial", Font.BOLD, 12));
+    
+    //Defining a JTextField object for user input
+    value = new JTextField();
+    value.setFont(new Font("Arial", Font.PLAIN, 16));
+    value.setBackground(Color.white);
+    value.setBorder(BorderFactory.createLineBorder(Color.black));
+    value.setPreferredSize(new Dimension(240, 40));
+    value.addActionListener(this);
+    inputOutputPanel.add(value);
+    contentPane.add(inputOutputPanel);
+    
+    //Defining a button object for activate operations
+    JButton button = new JButton("Calculate");
+    button.addActionListener(this);
+    buttonPanel.add(button);
+    contentPane.add(buttonPanel);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
         
-        public SinhUI()
+  /**
+   * Activates the user main action.
+   * @param event user action an event when click on the button.
+   */     
+  public void actionPerformed(ActionEvent event)
+  {
+    
+    /**
+     * Button action performed event
+     *
+     * @throws NumberFormatException If an I/O error occurs.
+     */
+    try {
+      if (event.getSource() instanceof JButton)
+      {
+        JButton clickedButton = (JButton) event.getSource();      
+        if (value.getText().isEmpty())
         {
-            
-          /*
-           * Creates Program Frame
-           */
-            Container contentPane = getContentPane();
-            contentPane.setLayout(new FlowLayout());
-            buttonPanel = new JPanel();
-            buttonPanel.setLayout(new GridLayout(7,7,9,9));
-            buttonPanel.setBorder(new EmptyBorder(12,12,12,12));
-
-            inputOutputPanel = new JPanel();
-            inputOutputPanel.setLayout(new FlowLayout());
-            inputOutputPanel.setBorder(new EmptyBorder(12,12,12,12));
-
-            setTitle("SINH Calculator");
-            setSize(FRAME_WIDTH,FRAME_HEIGHT);
-            setResizable(false);
-            setLocation(FRAME_X,FRAME_Y);
-            
-            /*
-             * Creates user input and 
-             * indication labels
-             */
-            
-            JLabel label = new JLabel();        
-            label.setText("SINH :");
-            label.setBounds(10, 10, 100, 100);
-            inputOutputPanel.add(label);
-            
-            /*
-             * Creates the Frame format
-             * for the text input
-             */
-            
-            value = new JTextField();
-            value.setFont(new Font("Arial", Font.PLAIN, 16));
-            value.setBackground(Color.white);
-            value.setBorder(BorderFactory.createLineBorder(Color.black));
-            value.setPreferredSize(new Dimension(240, 40));
-            value.addActionListener(this);
-            inputOutputPanel.add(value);
-            contentPane.add(inputOutputPanel);
+          JOptionPane.showMessageDialog(null, "System needs a value");
+        }
                 
-            /*
-             * Creates and formats the Calculate button
-             */
-            
-            JButton button = new JButton("Calculate");
-            button.addActionListener(this);
-            buttonPanel.add(button);
-            contentPane.add(buttonPanel);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-        }
-        
-        /*****************************************************************
-        Activates the user main action
-        @param event user action an event when click on the button
-        *****************************************************************/
-        
-        public void actionPerformed(ActionEvent event)
+        if (clickedButton.getText().equals("Calculate"))
         {
-            if (event.getSource() instanceof JButton)
-            {
-                JButton clickedButton = (JButton) event.getSource();
-                if(clickedButton.getText().equals("Calculate"))
-                {
-                    showResult();
-                    
-                }
-                else
-                {
-                    addValue(clickedButton.getText());
-                }
-            }
-            else
-            {
-                showResult();
-            }
+          showResult();            
         }
-        
-        /*****************************************************************
-        Method for setting values
-        @param line captures user's input
-        *****************************************************************/
-        
-        public void addValue(String line)
+        else
         {
-            if(editable)
-            {
-                value.setText(value.getText()+line);
-            }
-            else
-            {
-                value.setText(line);
-                editable = true;
-            }
+          addValue(clickedButton.getText());
         }
+      }
+      else
+      {
+        showResult();
+      }
+    }
+    catch (NumberFormatException e)
+    {
+      System.out.println("System needs a value");
+    }
+  }
+
+  /**
+   * Method for setting values.
+   * @param line captures user's input.
+   */       
+  public void addValue(String line)
+  {
+    if (editable)
+    {
+      value.setText(value.getText() + line);
+    }
+    else
+    {
+      value.setText(line);
+      editable = true;
+    }
+  }
         
-        /*****************************************************************
-        Method which pass the user input to the
-        logic methods to show the final result to the user
-        or a waring messages in case of wrong typos
-        *****************************************************************/
+  /**
+   * Method which pass the user input to the
+   * logic methods to show the final result to the user
+   * or a waring messages in case of wrong typos.
+   */
+  public void showResult()
+  {
+    
+    /**
+     * Show results event.
+     *
+     * @throws ScriptException If an script error occurs.
+     */
+    try
+    {
+      //Calling the main logic method to calculate the Sinh value
+      ScriptEngineManager mgr = new ScriptEngineManager();
+      ScriptEngine engine = mgr.getEngineByName("JavaScript");
+      valuex = Double.parseDouble(value.getText());
+      valueofx = SinhLogic.calculate_Sinh(valuex); 
+      finalxvalue = String.valueOf(valueofx);
+      value.setText(engine.eval(finalxvalue).toString());               
+    }
+    catch (ScriptException e)
+    {
+      value.setText("Syntax error");
+    }
+    editable = false;
+  }
         
-        public void showResult()
-        {
-            double output = 0;
-            try
-            {
-                ScriptEngineManager mgr = new ScriptEngineManager();
-                ScriptEngine engine = mgr.getEngineByName("JavaScript");
-                value_of_x = Double.parseDouble(value.getText());
-                x = SinhLogic.calculate_Sinh(value_of_x); 
-                val_of_x =String.valueOf(x);
-                value.setText(engine.eval(val_of_x).toString());               
-            }
-            catch(ScriptException e)
-            {
-                value.setText("Syntax error");
-            }
-            editable = false;
-        }
-        
-        /*****************************************************************
-        Program main method
-        which makes visible the user interface
-        @param args arguments to be passed
-        *****************************************************************/
-        
-        public static void main (String [] args)
-        {
-            SinhUI sinh = new SinhUI();
-            sinh.setVisible(true);
-        }
+
+  /**
+   * Program main method
+   * which makes visible the user interface.
+   * @param args arguments to be passed.     
+   */
+  public static void main(String [] args)
+  {
+    SinhUI sinh = new SinhUI();
+    sinh.setVisible(true);
+  }
 
 }
